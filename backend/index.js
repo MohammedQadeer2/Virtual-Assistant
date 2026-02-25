5416a7bc7508d73cbe570822cd5c459bf49c1eda
mohammed Qadeer <mr0060195@gmail.com>
2026-02-25 02:00:18 -0600

    fix assets

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,9 @@ import geminiResponse from "./Gemini.js";
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials: true
-}))
+  origin: true,
+  credentials: true
+}));
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth", authRouter)
